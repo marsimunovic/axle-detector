6d00775dc4a50326a124b7a62328c745bb481738
msimunovic <simunovic.marijo@gmail.com>
2015-07-22 08:52:53 +0200

    Add image source to ignore list

---
+++ src/main.m
@@ -0,0 +1,21 @@
+# this is main file of axle-detection project
+# it loads configuration environment
+# and calls appropriate functions accordingly
+
+# load configuration environment
+
+# get image list
+
+# for each image in list perform whole workflow
+
+# open image
+
+# crop unnecessary parts
+
+# filter bottom contour
+
+# analys bottom contour
+
+# perform detection
+
+# write results to file
