637b64fb52e38e171d3662cf226b6b17c59ca569
msimunovic <simunovic.marijo@gmail.com>
2015-07-23 15:21:16 +0200

    Adding edge detection functionality

---
+++ src/detect_edge.m
@@ -0,0 +1,41 @@
+function [ bottom_edge ] = detec_edge( input_image )
+%DETEC_EDGE detects lower boundary of the vehicle
+%   
+% input_image - matrix represantation of an image
+% bottom_edge - height at which first black pixel is found
+
+[height, width] = size(input_image);
+norm_image = input_image;
+bottom_edge = zeros(1, width);
+bottom_edge_filt = zeros(1, width);
+for n=1:width
+    updated = 0;
+    for m=height:-1:1
+        if(norm_image(m, n) == 0)
+            if(updated == 0)
+                bottom_edge(1,n) = height - m;
+                updated = 1;
+            end
+            if(sum(norm_image(m-3:m,n)) < 3)
+                bottom_edge_filt(1,n) = height - m;                
+                break; 
+            end
+        end
+    end
+end
+
+for n=1:width
+    if(bottom_edge(n) + 5 <= bottom_edge_filt(n))
+        bottom_edge_filt(n) = bottom_edge(n);
+    end
+end
+
+figure
+plot(bottom_edge)
+hold on
+plot(bottom_edge_filt, 'r')
+
+
+
+
+end
---
+++ src/image_edge.m
@@ -0,0 +1,37 @@
+function [ im_edge, im_half ] = image_edge( im_name )
+%IMAGE_EDGE detects edges in image 
+
+%   Edge detection is performed substracting
+%   original image and image shifted down by one
+%   
+%   im_name - input image name, eg. 'image1.gif'
+%   im_edge - detected edge in image
+%   im_half - lower half of im_edge
+close all
+LQ = imread(im_name);
+[h, w] = size(LQ);
+LQ((LQ > 0)) = 255;
+Empty = 255*ones(1, w);
+LQshift = [Empty; LQ(1:h, :)];
+ExtendLQ = [LQ; Empty];
+[height, width] = size(ExtendLQ);
+im_edge = zeros(height, width);
+for m=1:height
+    for n=1:width
+        if(ExtendLQ(m,n) ~= LQshift(m,n))
+            im_edge(m,n) = 0;
+        else
+            im_edge(m,n) = 255;
+        end
+    end
+end
+start = int32(height/2)+1;
+im_half = im_edge(start:end, :);
+filled_h = image_fill(im_half);
+filled = image_fill(filled_h);
+figure
+subplot(3,1,1), imshow(LQ(start:end, :))
+subplot(3,1,2), imshow(im_half)
+subplot(3,1,3), imshow(imsharpen(filled_h))
+
+end
