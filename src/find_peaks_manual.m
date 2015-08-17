83becefc7d52352205ece9a601af071114671f7c
msimunovic <simunovic.marijo@gmail.com>
2015-08-17 19:10:29 +0200

    Added proprietary function for minima and maxima detection

---
+++ src/find_peaks_manual.m
@@ -0,0 +1,43 @@
+function [peaks, peaks_min] = find_peaks_manual(input_data)
+%%find local maxima in input signal
+%%input_data - smooth input signal
+%%peaks - location indices of maxima values
+	peaks = [];
+	peaks_min = [];
+	ind = 1;
+	max_cand = input_data(1);
+	N = size(input_data,2);
+	n = 2;
+	while n <= N
+		if (max_cand > input_data(n))
+			peaks = [peaks ind];
+			#find next max_cand
+			while (input_data(n-1) > input_data(n)) && (n < N)
+				#do nothing
+				n = n + 1;
+			end
+			max_cand = input_data(n);
+			ind = n;
+		else
+			max_cand = input_data(n);
+			ind = n;
+		end
+		n = n + 1;
+	end
+	M = numel(peaks);
+	for n = 2:M
+		[M,I] = min(input_data(peaks(n-1):peaks(n)));
+		loc = I + peaks(n-1);
+		offset = 1;
+		while(loc+offset < peaks(n))
+			if(input_data(loc) == input_data(loc+offset))
+				offset = offset + 1;
+			else
+				break;
+			end
+		end
+		offset = int16(offset/2);
+		loc = loc + offset;
+		peaks_min = [peaks_min loc];
+	end
+end
